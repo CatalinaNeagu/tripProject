c45c847cd126ad77276aca37ff64bd3424b124b3
CatalinaNeagu <52670196+CatalinaNeagu@users.noreply.github.com>
2019-10-21 21:08:20 +0300

    Final

---
+++ src/main/java/ro/siit/trips/TripsApplication.java
@@ -0,0 +1,16 @@
+package ro.siit.trips;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsApplication.class, args);
+	}
+
+}
